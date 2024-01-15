d0b9ec44d63cfbba11810092d43a31708dafdd59
muzlive-info <117060680+muzlive-info@users.noreply.github.com>
2024-01-15 20:48:20 +0900

    init project

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KiTPlayerSDK",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "KiTPlayerSDK",
+            targets: ["KiTPlayerSDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "KiTPlayerSDK",
+            url: "https://github.com/muzlive-info/muzlive-kit-player-sdk-ios/releases/download/v1.0.0/KiTPlayerSDK.xcframework.zip",
+            checksum: "388bfcc6ae4ec1ac981195125724f5939efd71383eb2ae5f0e227b0157a6b749"
+        ),
+    ]
+)
