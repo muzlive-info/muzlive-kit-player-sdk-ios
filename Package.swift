ef3a0b6e83dc2d39b2f3561ccddb6169448dc874
Jaey.Lee <jy.lee@muzlive.com>
2024-02-28 10:41:14 +0900

    Release - v1.0.6

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KiTPlayerSDK",
-            url: "https://github.com/muzlive-info/muzlive-kit-player-sdk-ios/releases/download/1.0.5/KiTPlayerSDK.xcframework.zip",
-            checksum: "772dbee750bd50f8c133609f0093807d33b74952c79b3dc4f31f41cb93dceaf4"
+            url: "https://github.com/muzlive-info/muzlive-kit-player-sdk-ios/releases/download/v1.0.6/KiTPlayerSDK.xcframework.zip",
+            checksum: "eaaf2b8a333cc79ee5e88bb5c3c712d2265da9b398434f3db6d40e693b3f5c50"
         ),
     ]
 )
