69e747af9caf635975a23a8a8689e0e46aebed53
Jaey.Lee <jy.lee@muzlive.com>
2024-05-08 13:55:58 +0900

    Release - v1.0.17

--- Package.swift
+++ Package.swift
@@ -22,13 +22,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KiTPlayerSDK",
-            url: "https://github.com/muzlive-info/muzlive-kit-player-sdk-ios/releases/download/v1.0.16/KiTPlayerSDK.xcframework.zip",
-            checksum: "6ad594dd01fd05eb5b8d7e43bed63a07239b47fe060a2d654399bd3a4412b52a"
+            url: "https://github.com/muzlive-info/muzlive-kit-player-sdk-ios/releases/download/v1.0.17/KiTPlayerSDK.xcframework.zip",
+            checksum: "7c75411ce6ddec2228ba081a33aea90021c9a296982f93c2f253e14b75390247"
+        ),
+        .binaryTarget(
+            name: "GoogleCast",
+            path: "Sources/GoogleCast/GoogleCast.xcframework"
         ),
         .target(
             name: "KiTPlayerSDKTarget",
             dependencies: [
                 "KiTPlayerSDK",
+                "GoogleCast",
                 .product(name: "BitmovinPlayer", package: "player-ios"),
                 .product(name: "YouTubeiOSPlayerHelper", package: "youtube-ios-player-helper"),
 //                .product(name: "Lottie", package: "lottie-spm"),
